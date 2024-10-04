b7455ef0978e361ec58ba8e20843d9b6fe320ab8
Jadamec <jadamec@luleypharma.com>
2024-10-04 15:52:02 -0300

    functional testing pages with router

--- README.md
+++
@@ -1,6 +0,0 @@
-Lista de modificaciones a refactorizar: 
-.Paginacion de fetchPatientData (Mi UI)
-.Sin login
-.Pagina de error
-.Pasar toda la app a React.
-.
---
+++ osseg_react/src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import SiteRouter from './Routes/SiteRouter.tsx'
+import React from 'react'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+
+root.render(
+    <SiteRouter />
+)
