f1fc1c54ce887678a3f068acefff8b78e1037705
Jadamec <jadamec@luleypharma.com>
2024-10-04 16:54:55 -0300

    react redux guide

--- osseg_react/src/index.tsx
+++ osseg_react/src/index.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import SiteRouter from './Routes/SiteRouter.tsx'
 import React from 'react'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 )
 
 root.render(
+  <Provider store={store}>
     <SiteRouter />
+  </Provider>,
 )
